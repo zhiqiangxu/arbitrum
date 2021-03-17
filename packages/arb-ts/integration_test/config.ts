a93fade420cdc6895ffc40bc42d373fff8aa3456
dzgoldman <dzgoldman@wesleyan.edu>
2021-03-16 18:46:08 -0700

    add deposit eth and deposit erc20 tests, WIP

--- packages/arb-ts/integration_test/config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  ethRPC: 'https://devnet.arbitrum.io/rpc',
-  arbRPC: 'https://devnet-l2.arbitrum.io/rpc',
-  preFundedSignerPK: '',
-  erc20BridgeAddress: '',
-  arbTokenBridgeAddress: '',
-}
